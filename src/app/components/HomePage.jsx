49e3470964f2033343126f2dfd62584b23063f34
ubaid ghori <ubaid.razaghori@gmail.com>
2024-05-09 18:51:22 +0500

    updated

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
  <div className=' mt-36 mx-20 '>
   
   <div className=' flex flex-col justify-center items-center' >
-  <h1 className='text-[45px] md:text-[90px] text-white  font-bold'>
+  <h1 className='text-[35px] md:text-[90px] text-white text-center  font-bold'>
         Hi,I AM  <span className='text-[#73C2FB]'>UBAID</span>
       </h1>
      <TextEffect />
