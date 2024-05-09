a4d57c7ba12866874f43524eb6da597bb71d130e
ubaid ghori <ubaid.razaghori@gmail.com>
2024-05-09 16:43:37 +0500

    updated

--- src/app/components/Introduction.jsx
+++ src/app/components/Introduction.jsx
@@ -4,7 +4,7 @@ const Introduction = () => {
   return (
     <div className=' md:mt-48 mt-20 md:mx-20 mx-4'>
       <div  >
-       <Image src="https://scontent.fkhi22-1.fna.fbcdn.net/v/t39.30808-6/437104538_450567847342842_6434418363123287842_n.jpg?stp=c0.23.526.526a_dst-jpg_p526x296&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7Z0gL3Cfv_YQ7kNvgGwv2Xo&_nc_ht=scontent.fkhi22-1.fna&oh=00_AfDihq6NooQrA6fq5cAHp7P8_7aQMdPQ5X1hZLSrPaRMhQ&oe=6641535F" alt="" className=" w-[300px] h-[300px] border-2 rounded-full mx-auto" />
+       <Image src="/437104538_450567847342842_6434418363123287842_n.jpg" alt="" className=" w-[300px] h-[300px] border-2 rounded-full mx-auto" />
       </div>
       <div className=" mt-10  ">
         <h1 className=" font-bold text-[35px] text-[#73C2FB] font-mono">Introduction</h1>
