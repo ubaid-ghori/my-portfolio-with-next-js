ebcf8c962b4bcd98754283c2b6a2a3e96e8f4d9b
ubaid ghori <ubaid.razaghori@gmail.com>
2024-05-09 18:41:49 +0500

    updated

--- src/app/components/Introduction.jsx
+++ src/app/components/Introduction.jsx
@@ -18,7 +18,7 @@ const Introduction = () => {
       </div>
       <div className="mt-10">
         <h1 className="font-bold text-[35px] text-[#73C2FB] font-mono">Introduction</h1>
-        <p className="text-[20px] space-x-2 font-serif text-white mt-6 font-medium">Hey, I am Ubaid Raza, a dedicated <b className="text-[#73C2FB] mx-2 mr-2 font-bold text-[23px]">web enthusiast</b> who thrives on the ever-evolving landscape of digital creativity. My focus is on crafting intuitive and visually striking websites that leave a lasting impression. With a keen eye for design and a passion for clean code, I love bringing ideas to life in the digital realm. Let's collaborate and turn your vision into a captivating online experience!</p>
+        <p className="text-[20px] space-x-2 font-serif text-white mt-6 font-medium">Hey, I am Ubaid Raza, a dedicated <b className="text-[#73C2FB] mx-2 mr-2 font-bold text-[23px]">web enthusiast</b> who thrives on the ever-evolving landscape of digital creativity. My focus is on crafting intuitive and visually striking websites that leave a lasting impression. With a keen eye for design and a passion for clean code, I love bringing ideas to life in the digital realm. Lets collaborate and turn your vision into a captivating online experience!</p>
       </div>
       <div>
         <ul className="text-white mt-8 flex flex-col justify-center md:items-center gap-2 text-[17px]">
